ccedf21e35ba7838c301a74c173d62634b88b478
Khanyisile Ngoma <ngomakhanyi88@gmail.com>
2026-05-03 16:20:41 +0200

    KhanyiN Assessment

---
+++ src/test/java/commons/Paths.java
@@ -0,0 +1,11 @@
+package commons;
+
+//Database credentials and base URL for API testing are stored in this class as constants
+public class Paths {
+
+    public static final String BASE_URL = "https://ndosiautomation.co.za";
+    public static final String DB_URL = "jdbc:mysql://102.222.124.22:3306/ndosian6b8b7_teaching";
+    public static final String DB_USERNAME = "ndosian6b8b7_teaching";
+    public static final String DB_PASSWORD = "^{SF0a=#~[~p)@l1";
+
+}
